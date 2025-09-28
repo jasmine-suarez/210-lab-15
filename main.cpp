6763128dd9d5379f307755f7c9e71a77a5344423
Jasmine <jazzyysuarez@gmail.com>
2025-09-28 14:26:07 -0700

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,24 @@
 #include <iostream>
 using namespace std;
 
+class Movie {
+private:
+    string screenWriter;
+    int yrReleased;
+    string title;
+public:
+    string getScreenWriter()            { return screenWriter; }
+    void setScreenWriter(string s)      { screenWriter = s; }
+    int getYrReleased()                 { return yrReleased; }
+    void setYrReleased(int y)           { yrReleased = y; }
+    string getTitle()                   { return title; }
+    void setTitle(string t)             { title = t; }
+
+    void print() {
+        cout << "Movie: "
+    }
+};
+
 int main() {
 
     return 0;
